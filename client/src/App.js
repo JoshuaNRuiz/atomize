923f8b7538d6ea359443fa047ff2ec0f9a817bdb
Joshua N Ruiz <joshuanruiz@gmail.com>
2020-05-17 00:13:36 -0400

    more changes and some work on Login.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,30 @@
 import React, {useState, useEffect} from 'react';
 import Tracklist from './component/Tracklist/Tracklist.js'
+import Login from './container/Login.js'
 
 import './App.css';
+import Track from './component/Tracklist/Track/Track.js';
 
 function App() {
-  const [response, setResponse] = useState("");
-  
+  const [isLoggedIn, setLoginStatus] = useState(false);
+
+  if (!isLoggedIn) {
+
+  }
+
+  let authenticate = () => {
+    let clientId = '8e6f4d6f92d645d1b22ca1f6a8e8f371';
+    let uri = 'lvh.me:3000';
+    let scope = 'user-read-private';
+    let url = `https://accounts.spotify.com/authorize?client_id=${clientId}&scope=${scope}&response_type=token&redirect_uri=${uri}&show_dialog=false`
+    window.location.replace(url);
+  }
+
+  let container = isLoggedIn ? <Tracklist /> : <Login authenticate={authenticate}/>
+
   return (
     <div className="App">
-      <Tracklist />
+      {container} 
     </div>
   );
 }
--- client/src/component/Tracklist/Tracklist.js
+++ client/src/component/Tracklist/Tracklist.js
@@ -3,25 +3,25 @@ import Track from './Track/Track.js'
 
 const Tracklist = (props) => {
 
-    //GET https://api.spotify.com/v1/me/top/{type}
-
     const [type, setType] = useState('tracks');
     const [timeRange, setTimeRange] = useState('long_term')
     const [limit, setLimit] = useState(10);
     const [offset, setOffset] = useState(0)
-    const [response, setResponse] = useState(initialState);
+    const [response, setResponse] = useState();
 
-    let url = `https://api.spotify.com/v1/me/top/${type}
-            ?time_range=${timeRange}
-            &limit=${limit}
-            &offset=${offset}`
+    let tracklist = null;
+
+    let url = `https://api.spotify.com/v1/me/top/${type}?time_range=${timeRange}&limit=${limit}&offset=${offset}`
 
     useEffect(() => {
-        let result = fetch(url, {
+        // let result = fetch(url, {
+        //     method: 'get',
+        //     headers: new Headers({
 
-        });
-        setResponse(result);
-        generateTracklist();
+        //     }),
+        // });
+        // setResponse(result);
+        // generateTracklist();
     });
 
     let generateTracklist = () => {
@@ -30,13 +30,6 @@ const Tracklist = (props) => {
             return <Track name={track.name} artists={track.artists} album={track.album}/>
         });
     }
-
-    /* SPOTIFY API EXPECTED DATA
-        id: String
-        name: String
-        album: Album Object
-        artists: Artist object
-    */
 
     return (
         <div className="tracklist">
