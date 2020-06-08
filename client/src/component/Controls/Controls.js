c8f15cea20f3f1f7b7b83a8281d52df6386e882e
Joshua N Ruiz <joshuanruiz@gmail.com>
2020-06-07 23:46:16 -0400

    minor controls work

--- client/src/component/Controls/Controls.js
+++ client/src/component/Controls/Controls.js
@@ -4,13 +4,15 @@ import './Controls.css';
 const Controls = (props) => {
 
     return (
-        <div className='controls'> 
-            <select className='time-range-input' name="range" onChange={props.handleTimeRangeChange}>
-                <option value="long_term">All time</option>
-                <option value="medium_term">6 Months</option>
-                <option value="short_term">4 Weeks</option>
+        <div className='controls'>
+            <label for='time-range-input'>Time Range</label>
+            <select id='time-range-input' className='time-range-input' name='range' onChange={props.handleTimeRangeChange}>
+                <option value='long_term'>All time</option>
+                <option value='medium_term'>6 Months</option>
+                <option value='short_term'>4 Weeks</option>
             </select>
-            <input type='number' className='limit-input' min={props.min} max={props.max} value={props.limit} onClick={props.refresh} onChange={props.handleLimitChange}/>
+            <label for='limit-input'>#</label>
+            <input type='number' id='limit-input' className='limit-input' min={props.min} max={props.max} value={props.limit} onClick={props.refresh} onChange={props.handleLimitChange}/>
             <button className='refresh-button' onClick={props.handleRefresh}>Refresh</button>
         </div>
     )
