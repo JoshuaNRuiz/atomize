9b37255ee4bbf680c2041a25dd4ebbd5381c34f6
Joshua N Ruiz <joshuanruiz@gmail.com>
2020-09-21 17:38:57 -0400

    move frontend files to their own directory

---
+++ client/build/precache-manifest.3c6eff077b0c75f8d392fb8a548bb475.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0150ee968048e51e701d5ca42dff7674",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42ab1a7403d72b2a5c0f",
+    "url": "/static/css/main.40fcf4de.chunk.css"
+  },
+  {
+    "revision": "56b3e6005f0a455b288f",
+    "url": "/static/js/2.2c3e7a78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c3e7a78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42ab1a7403d72b2a5c0f",
+    "url": "/static/js/main.57f6cf6d.chunk.js"
+  },
+  {
+    "revision": "6547a36613c94c450fd6",
+    "url": "/static/js/runtime-main.11728364.js"
+  }
+]);
