9986f59c6e78c4fbbf97c3aeead09a95c5ffb445
Joshua N. Ruiz <joshuanruiz@gmail.com>
2021-08-09 21:35:18 -0400

    make port customizable

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 require('dotenv').config({path: __dirname + '/.env'});
 
+const DEFAULT_PORT = 8000;
+const BASE_PATH = process.env.BASE_PATH;
+const PORT = process.argv[2] || DEFAULT_PORT;
+
 const express = require('express');
 const app = express();
 const path = require('path');
 const cookieParser = require('cookie-parser');
-
-const BASE_PATH = process.env.BASE_PATH;
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(express.json());
@@ -17,6 +19,6 @@ app.get(BASE_PATH + '*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 })
 
-app.listen(process.env.PORT, () => {
-    console.log(`listening on port ${process.env.PORT}`);
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
 })
