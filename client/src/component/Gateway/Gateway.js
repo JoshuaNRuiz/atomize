7aca6dc53de81fdd0b0c533a5217cdbcabffda1d
Joshua N. Ruiz <joshuanruiz@gmail.com>
2021-09-24 21:41:25 -0400

    minor updates and temporary gateway

--- client/src/component/Gateway/Gateway.js
+++ client/src/component/Gateway/Gateway.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Header from '../Header/Header';
+import { Link } from 'react-router-dom';
 import './Gateway.css';
 
 const Gateway = () => {
 
     return (
-        <div>
-            
+        <div className="Gateway">
+            <Header>ATOMIZE</Header>
+            <Link to="/top" className="Part">
+                <i className="fas fa-trophy Part__Icon" />
+                <span className="Part__Title"> Top Artists & Tracks</span>
+            </Link>
         </div>
     )
 
